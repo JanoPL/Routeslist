fc2b10cf2e0c891ad5a9f8ed93a0f0b24960408d
Janusz <xjanusz4@gmail.com>
2020-06-15 17:24:00 +0200

    [Solution] - changed version - added new project - move project to new directory

---
+++ RoutesList-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoutesList_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
