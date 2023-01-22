dc5e34acf00421da8fcd43190f0851b6b75870b1
JanoPL <xjanusz4@gmail.com>
2023-01-22 15:20:35 +0100

    updated coverage script - bump version - added remove directory TestResults - added restore packages

--- coverage_test.ps1
+++ coverage_test.ps1
@@ -3,25 +3,30 @@
     Build and generate coverate raport for tests
 
 .DESCRIPTION
-    To build and test, this script is required put reportgenerator and coverrage collector module 
+    To build and test, this script required reportgenerator and coverrage collector module to run
 
 .EXAMPLE
     Build_&_pack.ps1
 
 .NOTES
-    Version    : 1.0.0
+    Version    : 1.1.0
     Author     : JanoPL
-    Created on : 2022-12-19
+    Created on : 2023-01-22
     License    : MIT License
-    Copyright  : (c) 2022 JanoPL
+    Copyright  : (c) 2023 JanoPL
 #>
 
-dotnet build .\RoutesList.sln --configuration Release
+dotnet restore;
 
-dotnet test --collect:"XPlat Code Coverage" --no-build 
+dotnet build .\RoutesList.sln --configuration Debug;
+
+dotnet test --collect:"XPlat Code Coverage" --no-build;
 
 if (Get-Command reportgenerator.exe -ErrorAction SilentlyContinue) {
-    reportgenerator.exe -reports:tests\*\TestResults\*\coverage.cobertura.xml -targetdir:coveragereport    
+    reportgenerator.exe -reports:tests\*\TestResults\*\coverage.cobertura.xml -targetdir:coveragereport;
 } else {
-    Write-Host "Report generator does'n exist, please install via 'dotnet tool install -g dotnet-reportgenerator-globaltool' "
+    Write-Host "Report generator does'n exist, please install via 'dotnet tool install -g dotnet-reportgenerator-globaltool'";
 }
+
+Write-Host "Removing Directory TestResults";
+Remove-Item tests\*\TestResults -Recurse -Verbose;
