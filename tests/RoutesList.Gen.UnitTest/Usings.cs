661b1173bb76f8329f311c8bfff3ec05d97a7313
JanoPL <xjanusz4@gmail.com>
2023-02-04 01:58:04 +0100

    removing files from repo

--- tests/RoutesList.Gen.UnitTest/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using RoutesList.Build.Services.StaticFileBuilder;
-global using RoutesList.Interfaces;
-global using Microsoft.AspNetCore.Builder;
-global using FluentAssertions;
-global using Microsoft.AspNetCore.Http;
