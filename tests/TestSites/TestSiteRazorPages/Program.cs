ec93fb852c483ff185cfff0db63be51b47fc226f
JanoPL <xjanusz4@gmail.com>
2024-05-20 21:36:16 +0200

    [test] #92 add test for razor pages with .NET8.0

--- tests/TestSites/TestSiteRazorPages/Program.cs
+++ tests/TestSites/TestSiteRazorPages/Program.cs
@@ -5,14 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddControllers();
+
 builder.Services.AddRoutesList();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Error");
-}
+if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Error");
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -23,11 +23,16 @@ app.MapRazorPages();
 
 IDictionary<string, string[]> dict = new Dictionary<string, string[]>();
 
-string[] classes = dict["table"] = new string[2] { "table", "table-striped" };
+var classes = dict["table"] = new string[2] { "table", "table-striped" };
 app.UseRoutesList(options => {
     options.Tittle = "Title";
     options.Endpoint = "routes";
     options.SetTableClasses(classes);
 });
 
+app.MapControllers();
 app.Run();
+
+public partial class Program
+{
+};
